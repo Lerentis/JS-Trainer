3557eae8e541f7c392416f0ab098618aff56d0af
Tanja Kollarczik <sttng1511@gmail.com>
2015-12-08 22:32:15 +0100

    minor changes

--- docker/docker-mysql/build_and_run.sh
+++ docker/docker-mysql/build_and_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t js_trainer_mysql .
-docker run --name js_trainer_mysql -e MYSQL_ROOT_PASSWORD="V5k6hBk~5:<s7q" --link 427b2c6b1a51:mysql -p 3306:3306 -d js_trainer_mysql
+docker run --name js_trainer_mysql -e MYSQL_ROOT_PASSWORD="V5k6hBk~5:<s7q" --link phpbb:mysql -p 3306:3306 -d js_trainer_mysql
