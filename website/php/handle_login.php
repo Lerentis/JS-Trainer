829845003995839fa50c314f6bf7db0d9912073d
lerentis <lerentis@burntbunch.org>
2015-12-16 11:45:01 +0100

    fixed path in /php/handle_login.php ... sorry ._.

--- website/php/handle_login.php
+++ website/php/handle_login.php
@@ -2,7 +2,7 @@
 		ini_set('display_errors',1); ini_set('display_startup_errors',1);
 
 		define('IN_PHPBB', true);
-		$phpbb_root_path = '/usr/share/nginx/html/docker/docker-phpbb/app/phpBB3/';
+		$phpbb_root_path = '/usr/share/phpBB3/';
 		$phpEx = substr(strrchr(__FILE__, '.'), 1);
 		include($phpbb_root_path . 'common.' . $phpEx);
 
