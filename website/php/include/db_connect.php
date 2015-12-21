a93533521d05cc9ed0772afa722fdcb2156f9980
Tanja Kollarczik <sttng1511@gmail.com>
2015-12-21 16:07:38 +0100

    Tutorials

--- website/php/include/db_connect.php
+++ website/php/include/db_connect.php
@@ -17,32 +17,36 @@ class db{
         $this->conn = new mysql();
         // we're using bertie and bertiezilla as our example user credentials. You need to fill in your own ;D
         $this->conn->sql_connect(HOST, USER, PASSWORD, DATABASE, '', false, false);
+
+        echo $this->conn->hostinfo();
     }
 
-    function __destruct()
-    {
-        $this->conn->sql_close();
-    }
 
-    function getProgress($user_id){
-        //Array with the data to insert
-        $data = array(
-            'user_id'     => '2'
-        );
 
-        // First doing a select with this data.
-        // Note: By using the SELECT type, it uses always AND in the query.
-        $sql = 'SELECT user_id FROM  user_progress WHERE ' . $this->conn->sql_build_array('SELECT', $data);
-        $result = $this->conn->sql_query($sql);
-
-        var_dump($result);
-
-        return $result;
-
-        //$sql = "SELECT progress from user_progress WHERE user_id=(" . $user_id . ");" ;
-
-    }
+//    function __destruct()
+//    {
+//        $this->conn->sql_close();
+//    }
+//
+//    function getProgress($user_id){
+//        //Array with the data to insert
+//        $data = array(
+//            'user_id'     => '2'
+//        );
+//
+//        // First doing a select with this data.
+//        // Note: By using the SELECT type, it uses always AND in the query.
+//        $sql = 'SELECT user_id FROM  user_progress WHERE ' . $this->conn->sql_build_array('SELECT', $data);
+//        $result = $this->conn->sql_query($sql);
+//
+//        var_dump($result);
+//
+//        return $result;
+//
+//        //$sql = "SELECT progress from user_progress WHERE user_id=(" . $user_id . ");" ;
+//
+//    }
 
 }
 
-?>
+$mydb = new db();
