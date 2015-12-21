ce312c2b4323436e79fe5da78baabfdeac890edb
lerentis <lerentis@burntbunch.org>
2015-12-21 15:00:28 +0100

    started database abstraction layer implementation

--- website/php/include/db_connect.php
+++ website/php/include/db_connect.php
@@ -1,5 +1,48 @@
 <?php
-	ini_set('display_errors',1); ini_set('display_startup_errors',1);
-	include_once 'psl-conf.php';
-	$conn=new mysqli(HOST, USER, PASSWORD, DATABASE);
+ini_set('display_errors',1); ini_set('display_startup_errors',1);
+define('IN_PHPBB', true);
+$phpbb_root_path = '/usr/share/phpBB3/';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+include($phpbb_root_path . 'phpbb/db/driver/mysql.' . $phpEx);
+include_once 'psl-conf.php';
+
+class db{
+
+    private $conn;
+
+    function __construct()
+    {
+        //$this->conn = new mysqli(HOST, USER, PASSWORD, DATABASE);
+        $this->conn = new mysql();
+        // we're using bertie and bertiezilla as our example user credentials. You need to fill in your own ;D
+        $this->conn->sql_connect(HOST, USER, PASSWORD, DATABASE, '', false, false);
+    }
+
+    function __destruct()
+    {
+        $this->conn->sql_close();
+    }
+
+    function getProgress($user_id){
+        //Array with the data to insert
+        $data = array(
+            'user_id'     => '2'
+        );
+
+        // First doing a select with this data.
+        // Note: By using the SELECT type, it uses always AND in the query.
+        $sql = 'SELECT user_id FROM  user_progress WHERE ' . $this->conn->sql_build_array('SELECT', $data);
+        $result = $this->conn->sql_query($sql);
+
+        var_dump($result);
+
+        return $result;
+
+        //$sql = "SELECT progress from user_progress WHERE user_id=(" . $user_id . ");" ;
+
+    }
+
+}
+
 ?>
--- website/test.php
+++ website/test.php
@@ -1,29 +1,14 @@
 <?php
 ini_set('display_errors',1); ini_set('display_startup_errors',1);
 
-define('IN_PHPBB', true);
-$phpbb_root_path = '/usr/share/phpBB3/';
-$phpEx = substr(strrchr(__FILE__, '.'), 1);
-include($phpbb_root_path . 'common.' . $phpEx);
-include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
+echo "Test0";
 
-// Start session management
-$user->session_begin();
-$auth->acl($user->data);
-$user->setup();
+//include ("php/include/db_connect.php");
 
-//first we get the users avatar that is viewing this page but only if they are logged in and have one?
-if ($user->data['is_registered'] && !$user->data['is_bot'])
-{
-    $user_avatar_img = !empty($user->data['user_avatar']) ? get_user_avatar($user->data['user_avatar'], $user->data['user_avatar_type'], $user->data['user_avatar_width'], $user->data['user_avatar_height']) : '';
-    if(!empty($user_avatar_img))
-    {
-        echo $user->data['user_avatar'];
-        echo get_user_avatar($user->data['user_avatar'], $user->data['user_avatar_type'],$user->data['user_avatar_width'], $user->data['user_avatar_height']);
-        echo $user_avatar_img;
-    }
-}
-else {
-    echo "Not logged in";
-}
+//$db = new db();
 
+//$res = $db->getProgress(2);
+
+//var_dump($res);
+
+?>
