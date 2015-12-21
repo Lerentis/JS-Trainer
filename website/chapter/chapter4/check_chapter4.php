e54dc679f5bcd749848d4f2b8ba00410042870cf
Tanja Kollarczik <sttng1511@gmail.com>
2015-12-21 22:46:12 +0100

    quizz mask

--- website/chapter/chapter4/check_chapter4.php
+++ website/chapter/chapter4/check_chapter4.php
@@ -5,7 +5,7 @@ define('IN_PHPBB', true);
 $phpbb_root_path = '/usr/share/phpBB3/';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
-
+include('../../php/include/db_connect.php');
 // Start session management
 $user->session_begin();
 $auth->acl($user->data);
@@ -13,10 +13,23 @@ $user->setup();
 
 if($user->data['is_registered']){
 
+    //var_dump($_POST);
+   $array=$request->variable(answer,array(answer,1),NULL);
+
+    $array_sum=0;
+
+
+
+    for($i=0;$i<count($array);$i++){
+        $array_sum+=intval($array[$i]);
+    }
+
+    $db = new db();
+    $db->tutorialCompleted($user->data['user_id'],4,$array_sum*10);
+
 ?>
 <!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
-<html class="no-js" lang="en" data-useragent="Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)"
-      xmlns="http://www.w3.org/1999/html">
+<html class="no-js" lang="en" data-useragent="Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)">
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -26,7 +39,7 @@ if($user->data['is_registered']){
     <script src="../../js/vendor/jquery.js"></script>
     <script src="../../js/foundation.min.js"></script>
     <script src="../../js/vendor/jquery.cookie.js"></script>
-    <title> <?php echo "Chapter 4 for " . $user->data['username']; ?> </title>
+    <title> <?php echo "Quizz 4 for " . $user->data['username']; ?> </title>
 </head>
 <body>
 <div class="row">
@@ -36,7 +49,7 @@ if($user->data['is_registered']){
                 <nav class="top-bar" data-topbar>
                     <ul class="title-area">
                         <li class="name">
-                            <h1><a href="user.php">Learn JavaScript within minutes</a></h1>
+                            <h1><a href="../../user.php">Learn JavaScript within minutes</a></h1>
                         </li>
                         <li class="toggle-topbar menu-icon">
                             <a href="#"><span>menu</span></a>
@@ -125,45 +138,50 @@ if($user->data['is_registered']){
         <div class="split">
             <br>
         </div>
-        <h1>Loops a round thing</h1>
+        <div class="split">
+            <br>
+        </div>
+        <article>
+            <?php echo "You've got ".$array_sum." points in your Quizz!"?>
+        </article>
+
+
+        <div class="split">
+            <br>
+        </div>
+        <div class="row">
+            <div class="small-3 columns small-centered">
+                <a  href="live_editor_chapter1_part1.php" target="_blank">
+                    <div class="button primary round radius text-center expand">
+                        Try It!
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="split">
+            <br>
+        </div>
+        <article>
+
+        </article>
         <div class="split">
             <br>
         </div>
 
-
-        <form method="post" action="check_chapter4.php">
-            <lable for "1" ><b>Question 1:Where ist plain javascript executed?</b></lable>
-            <br>
-            <input type="radio" name="answer[1]" value=0 > On the server side.
-            <br>
-            <input type="radio" name="answer[1]" value=1 > In the browser from the client.
-            <br>
-            <input type="radio" name="answer[1]" value=0 > It's a script language which doesn't need to be executed
-            <br>
-            <input type="radio" name="answer[1]" value=0 > On server an client side.
-            <br>
-            <br>
-            <lable for "question2" ><b>Question 2:What can you manipulate with javascript?(multi)</b></lable>
-            <br>
-            <input type="radio" name="answer[2]" value=1 > html attributes, tags and content.
-            <br>
-            <input type="radio" name="answer[3]" value=1 > CSS style.
-            <br>
-            <input type="radio" name="answer[4]" value=0 > The server.
-            <br>
-            <input type="radio" name="answer[5]" value=1 > The logic of a side.
-            <br>
-            <br>
-
-            <br>
-            <br>
             <div class="row">
                 <div class="columns pagination-centered">
-                    <input type="submit" class="button round" value="Check your answers!">
+                    <ul class="button-group round ">
+                        <li>
+                            <a href="#" class="button secondary" >Test Knowledge</a>
+                        </li>
+
+                        <li>
+                            <a href="../chapter2/tutorial_chapter2.php" class="button secondary">Next Chapter</a>
+                        </li>
+                    </ul>
+
                 </div>
             </div>
-
-        </form>
 
         <footer class="row">
             <div class="large-12 columns">
